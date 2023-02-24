4fedde987708ab97b7d3ae96de764516236a04b3
mapoy62 <oyuyumap@gmail.com>
2023-02-24 15:24:35 -0600

    Agregar docuemento - Scrum & def git

--- SemanaUno/src/com/polimorfismo/Discapacidad.java
+++ SemanaUno/src/com/polimorfismo/Discapacidad.java
@@ -1,7 +1,8 @@
 package com.polimorfismo;
 
 public class Discapacidad {
-		
+	
+		//Definición de métodos en clase padre
 		void definirDiscap() {
 			System.out.println("Condición de vida que tiene ciertas limitaciones sociales para vivir una vida plena.");
 		}
--- SemanaUno/src/com/polimorfismo/Principal.java
+++ SemanaUno/src/com/polimorfismo/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		//Persona que apunta a un tipo de discapacidad
 		Discapacidad persona = getPersona();
 		
 		System.out.println(persona.getClass().getSimpleName());
@@ -13,6 +14,7 @@ public class Principal {
 		persona.asignarHerramienta();
 	}
 		
+		//Método que asigna discapacidad a persona
 		private static Discapacidad getPersona() {
 			Discapacidad[] persona = { new DiscVisual(), new DiscAuditiva(), new DiscFisica() };
 			int entero = new Random().nextInt(persona.length);
